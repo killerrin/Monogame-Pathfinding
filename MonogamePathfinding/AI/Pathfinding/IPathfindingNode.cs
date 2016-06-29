ce23a314f0d34c52df9aeabe9835552192b413da
Andrew Godfroy <andrew.godfroy@killerrin.com>
2016-06-29 00:25:51 -0400

    Added Pathfinding Extensions

--- MonogamePathfinding/AI/Pathfinding/IPathfindingNode.cs
+++ MonogamePathfinding/AI/Pathfinding/IPathfindingNode.cs
@@ -12,21 +12,4 @@ namespace MonogamePathfinding.AI.Pathfinding
         IPathfindingNode Parent { get; set; }
         IGridNode GridNode { get; }
     }
-
-    public static class PathfindingExtensions
-    {
-        public static IPathfindingNode Reverse(this IPathfindingNode root)
-        {
-            IPathfindingNode p = root, n = null;
-            while (p != null)
-            {
-                IPathfindingNode tmp = p.Parent;
-                p.Parent = n;
-                n = p;
-                p = tmp;
-            }
-            root = n;
-            return root;
-        }
-    }
 }
---
+++ MonogamePathfinding/AI/Pathfinding/PathfindingExtensions.cs
@@ -0,0 +1,26 @@
+﻿using MonogamePathfinding.AI.Pathfinding.Grid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonogamePathfinding.AI.Pathfinding
+{
+    public static class PathfindingExtensions
+    {
+        public static IPathfindingNode Reverse(this IPathfindingNode root)
+        {
+            IPathfindingNode p = root, n = null;
+            while (p != null)
+            {
+                IPathfindingNode tmp = p.Parent;
+                p.Parent = n;
+                n = p;
+                p = tmp;
+            }
+            root = n;
+            return root;
+        }
+    }
+}
