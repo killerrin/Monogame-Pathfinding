62419ab29225302a7fca7d5de069834edb06b737
Andrew Godfroy <andrew.godfroy@killerrin.com>
2016-06-29 17:34:29 -0400

    Shaved 200ms from A* Time
    Swapped the ClosedList from a List to a Dictionary

--- MonogamePathfinding/AI/Pathfinding/Engines/AStarPathfindingEngine.cs
+++ MonogamePathfinding/AI/Pathfinding/Engines/AStarPathfindingEngine.cs
@@ -40,10 +40,12 @@ namespace MonogamePathfinding.AI.Pathfinding.Engines
         {
             if (!Grid.WithinGrid(startPosition)) return null;
             if (!Grid.WithinGrid(endPosition)) return null;
+            
 
             // Create the opened and closed lists
-            List<IPathfindingNode> closedList = new List<IPathfindingNode>();
-            PriorityQueue<PriorityQueueNode<IPathfindingNode>> openedList = new PriorityQueue<PriorityQueueNode<IPathfindingNode>>();
+            Dictionary<UInt64, IPathfindingNode> closedList = new Dictionary<ulong, IPathfindingNode>();
+            BinaryHeap<float, IPathfindingNode> openedList = BinaryHeap<float, IPathfindingNode>.MinBinaryHeap(4);
+            //PriorityQueue<PriorityQueueNode<IPathfindingNode>> openedList = new PriorityQueue<PriorityQueueNode<IPathfindingNode>>();
 
             // Cache the End Node
             IGridNode endingGridNode = Grid.FindNode(endPosition);
@@ -52,13 +54,15 @@ namespace MonogamePathfinding.AI.Pathfinding.Engines
             // Get the starting node and add it to the opened list
             IGridNode startingGridNode = Grid.FindNode(startPosition);
             IPathfindingNode startingPathfindingNode = new PathfindingNode(startingGridNode);
-            openedList.Enqueue(new PriorityQueueNode<IPathfindingNode>(0, startingPathfindingNode));
+            openedList.Insert(0.0f, startingPathfindingNode);
+            //openedList.Enqueue(new PriorityQueueNode<IPathfindingNode>(0, startingPathfindingNode));
 
             // Begin Pathfind
             IPathfindingNode currentNode = null;
             while (openedList.Count > 0)
             {
-                currentNode = openedList.Dequeue().Data;
+                currentNode = openedList.Pop().Data;
+                //currentNode = openedList.Dequeue().Data;
 
                 // Get all of the Adjacent Nodes to our Current Node
                 var adjacentGridNodes = Grid.GetAdjacentNodes(currentNode.GridNode.Position, AllowHorizontalVerticalMovement, AllowDiagonalMovement);
@@ -80,7 +84,8 @@ namespace MonogamePathfinding.AI.Pathfinding.Engines
                     {
                         endingPathfindingNode.Parent = currentNode;
 
-                        var result = new PathfindingResult(endingPathfindingNode, closedList, openedList.Select(x => x.Data));
+                        //var result = new PathfindingResult(endingPathfindingNode, closedList, openedList.Select(x => x.Data));
+                        var result = new PathfindingResult(endingPathfindingNode, closedList.Values, openedList);
 
                         if (PathFound != null)
                             PathFound(this, new PathfindingEventArgs(result));
@@ -88,7 +93,7 @@ namespace MonogamePathfinding.AI.Pathfinding.Engines
                         return result;
                     }
 
-                    if (!ClosedListContains(closedList, node))
+                    if (!closedList.ContainsKey(node.GridNode.Key()))
                     {
                         node.Parent = currentNode;
 
@@ -111,46 +116,34 @@ namespace MonogamePathfinding.AI.Pathfinding.Engines
 
                 }
 
-                AddToClosedList(closedList, currentNode);
+                if (!closedList.ContainsKey(currentNode.GridNode.Key()))
+                    closedList[currentNode.GridNode.Key()] = currentNode;
             }
 
             // Sadly, there is no path to our target so we return null
-            return new PathfindingResult(null, closedList, new List<IPathfindingNode>());
+            return new PathfindingResult(null, closedList.Values, new List<IPathfindingNode>());
         }
 
         #region Opened/Closed List Helper Methods
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void AddToOpenedList(PriorityQueue<PriorityQueueNode<IPathfindingNode>> openedList, IPathfindingNode node, IPathfindingNode endNode)
+        private void AddToOpenedList(BinaryHeap<float, IPathfindingNode> openedList, IPathfindingNode node, IPathfindingNode endNode)
         {
             float heuristic = Heuristic.CalculateHeuristic(node.GridNode.Position, endNode.GridNode.Position,
                 node.GridNode.MovementCost + BaseMovementCost,
                 node.GridNode.MovementCost + BaseDiagonalMovementCost);
             Debug.WriteLine($"Heuristic: {heuristic} | MovementCost: {node.GridNode.MovementCost} | BaseMovement: {BaseMovementCost} | DiagonalMovement: {BaseDiagonalMovementCost}");
 
-            openedList.Enqueue(new PriorityQueueNode<IPathfindingNode>((int)heuristic, node));
+            //openedList.Enqueue(new PriorityQueueNode<IPathfindingNode>((int)heuristic, node));
+            openedList.Insert(heuristic, node);
         }
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private void AddToClosedList(List<IPathfindingNode> closedList, IPathfindingNode node)
-        {
-            if (!ClosedListContains(closedList, node))
-                closedList.Add(node);
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private bool OpenedListContains(PriorityQueue<PriorityQueueNode<IPathfindingNode>> openedList, IPathfindingNode node)
+        private bool OpenedListContains(BinaryHeap<float, IPathfindingNode> openedList, IPathfindingNode node)
         {
             for (int i = 0; i < openedList.Count; i++)
             {
+                if (openedList[i] == null) continue;
+
                 if (openedList[i].Data.GridNode.Position == node.GridNode.Position)
-                    return true;
-            }
-            return false;
-        }
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        private bool ClosedListContains(List<IPathfindingNode> closedList, IPathfindingNode node)
-        {
-            for (int i = 0; i < closedList.Count; i++)
-            {
-                if (closedList[i].GridNode.Position == node.GridNode.Position)
                     return true;
             }
             return false;
